e3df8ca40f892f7d9285fbceda492e4301856370
braulio.ruiz <braulio.ruiz@outlook.cl>
2024-07-24 10:50:58 -0400

    modified:   assets/js/main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
 // Archivo JavaScript principal para funcionalidades adicionales
+
+// Muestra un mensaje en la consola del navegador indicando que el archivo main.js se ha cargado correctamente
 console.log('Archivo main.js cargado correctamente.');
+
+// Muestra un mensaje en la consola del navegador para dar la bienvenida y mencionar el proyecto y curso
 console.log('Que disfrutes mi sitio "miproyecto", Programaciom Web IPP 2024.');
