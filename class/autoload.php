6d2947fea208f538786aad6157fe27c1453f6af0
braulio.ruiz <braulio.ruiz@outlook.cl>
2024-08-06 10:01:37 -0400

    modified:   class/autoload.php

--- class/autoload.php
+++ class/autoload.php
@@ -5,7 +5,6 @@
 // La función de autoload toma el nombre de la clase que se está intentando utilizar ($class_name)
 spl_autoload_register(function ($class_name) {
     // Incluye el archivo que contiene la definición de la clase
-    // Supone que el archivo se llama igual que la clase y tiene una extensión .php
     // Esto significa que si se intenta usar una clase llamada 'Producto',
     // PHP intentará incluir un archivo llamado 'Producto.php'
     include $class_name . '.php';
