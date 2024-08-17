1234cc0265f52b9e9aab52c5134053f9cafa4de5
Braulio Ruiz <braulio.ruiz@outlook.cl>
2024-08-17 19:52:39 -0400

    modified:   assets/css/estilos.css 	modified:   assets/js/jquerySlim.js 	modified:   assets/js/main.js 	modified:   assets/js/validaciones.js 	modified:   backend/views/categorias.html

--- assets/js/validaciones.js
+++ assets/js/validaciones.js
@@ -1,36 +1,33 @@
 // Esta función se ejecuta cuando el documento HTML está completamente cargado y listo.
 $(document).ready(function () {
-  // Maneja los eventos 'keyup' y 'blur' en los campos de entrada dentro de los formularios especificados
-  $("#formCategorias, #formProductos").on("keyup blur", "input", function () {
-    var $this = $(this); // Convierte el campo de entrada actual en un objeto jQuery
-    if ($this.val().trim() === "") {
-      // Verifica si el campo está vacío después de eliminar espacios en blanco
-      $this.addClass("error"); // Agrega la clase 'error' al campo
-    } else {
-      $this.removeClass("error"); // Elimina la clase 'error' si el campo no está vacío
-    }
-  });
-  // Maneja el evento 'click' en el botón con clase 'btn-cancelar'
-  $(".btn-cancelar").on("click", function (event) {
-    event.preventDefault(); // Previene el comportamiento por defecto del botón (enviar una solicitud o seguir un enlace)
-    // Muestra un cuadro de confirmación al usuario
-    if (confirm("¿Estás seguro de que quieres cancelar?")) {
-      // Redirige al usuario a la página principal si confirma la acción
-      window.location.href = "../../index.php";
-    }
-  });
+    // Maneja los eventos 'keyup' y 'blur' en los campos de entrada dentro de los formularios especificados
+    $("#formCategorias, #formProductos").on('keyup blur', 'input', function () {
+        var $this = $(this); // Convierte el campo de entrada actual en un objeto jQuery
+        if ($this.val().trim() === "") { // Verifica si el campo está vacío después de eliminar espacios en blanco
+            $this.addClass('error'); // Agrega la clase 'error' al campo
+        } else {
+            $this.removeClass('error'); // Elimina la clase 'error' si el campo no está vacío
+        }
+    });
+    // Maneja el evento 'click' en el botón con clase 'btn-cancelar'
+    $('.btn-cancelar').on('click', function (event) {
+        event.preventDefault(); // Previene el comportamiento por defecto del botón (enviar una solicitud o seguir un enlace)
+        // Muestra un cuadro de confirmación al usuario
+        if (confirm('¿Estás seguro de que quieres cancelar?')) {
+            // Redirige al usuario a la página principal si confirma la acción
+            window.location.href = '../../index.php';
+        }
+    });
 });
 
 // Función para eliminar una categoría
 function eliminarCategoria() {
-  // Confirmación antes de proceder
-  if (confirm("¿Estás seguro de que deseas eliminar esta categoría?")) {
-  }
+    // Confirmación antes de proceder
+    if (confirm('¿Estás seguro de que deseas eliminar esta categoría?')) { }
 }
 
 // Función para eliminar un producto
 function eliminarProducto() {
-  // Confirmación antes de proceder
-  if (confirm("¿Estás seguro de que deseas eliminar este producto?")) {
-  }
+    // Confirmación antes de proceder
+    if (confirm('¿Estás seguro de que deseas eliminar este producto?')) { }
 }
