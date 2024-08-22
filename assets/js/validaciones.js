21adf19906a6c2979d3d9b56a093f3c70cf7adda
braulio.ruiz <braulio.ruiz@outlook.cl>
2024-08-22 10:40:54 -0400

    modified:   assets/css/estilos.css 	modified:   assets/js/jquery-3.7.1.min.js 	modified:   assets/js/main.js 	modified:   assets/js/validaciones.js 	modified:   backend/categorias.php 	modified:   backend/productos.php 	modified:   backend/views/categorias.html 	deleted:    backend/views/editar_categoria.php 	modified:   backend/views/lista_categorias.php 	modified:   backend/views/lista_productos.php 	modified:   backend/views/productos.php 	modified:   class/autoload.php 	modified:   class/categorias.php 	modified:   class/database.php 	modified:   class/productos.php 	modified:   index.php 	modified:   views/home.php

--- assets/js/validaciones.js
+++ assets/js/validaciones.js
@@ -2,77 +2,94 @@
 
 // Esta función se ejecuta cuando el documento HTML está completamente cargado y listo.
 $(document).ready(function () {
-    // Define una función para validar los campos del formulario
-    function validarCampos(campos) {
-        var errores = []; // Crea un array para almacenar los nombres de los campos con errores
-        // Itera sobre cada campo pasado a la función
-        campos.each(function () {
-            var $this = $(this); // Convierte el elemento actual en un objeto jQuery
-            if ($this.val().trim() === "") { // Verifica si el valor del campo está vacío después de eliminar espacios en blanco
-                $this.addClass('error'); // Agrega la clase 'error' al campo para indicar un error
-                // Agrega el nombre del campo (con formato mejorado) al array de errores
-                errores.push(
-                    $this.attr('name') // Obtiene el valor del atributo 'name' del campo
-                        .replace(/_/g, ' ') // Reemplaza los guiones bajos con espacios
-                        .replace(/\b\w/g, l => l.toUpperCase()) // Capitaliza la primera letra de cada palabra
-                );
-            } else {
-                $this.removeClass('error'); // Elimina la clase 'error' si el campo no está vacío
-            }
-        });
-        // Devuelve el array de errores
-        return errores;
-    }
-    // Maneja los eventos 'keyup' y 'blur' en los campos de entrada dentro de los formularios especificados
-    $("#formCategorias, #formProductos").on('keyup blur', 'input', function () {
-        var $this = $(this); // Convierte el campo de entrada actual en un objeto jQuery
-        if ($this.val().trim() === "") { // Verifica si el campo está vacío después de eliminar espacios en blanco
-            $this.addClass('error'); // Agrega la clase 'error' al campo
-        } else {
-            $this.removeClass('error'); // Elimina la clase 'error' si el campo no está vacío
-        }
-    });
-    // Maneja el evento 'submit' del formulario con id 'formCategorias'
-    $("#formCategorias").submit(function (event) {
-        event.preventDefault(); // Previene el comportamiento por defecto del formulario (enviar la solicitud)
-        var errores = validarCampos($("#nombreCategoria")); // Llama a la función de validación para el campo 'nombreCategoria'
-        if (errores.length > 0) { // Verifica si hay errores
-            alert("Completar los siguientes campos: " + errores.join(", ") + " de la categoria."); // Muestra una alerta con los campos faltantes
-            return false; // Previene el envío del formulario
-        }
-        this.submit(); // Envía el formulario si no hay errores
-    });
-    // Maneja el evento 'submit' del formulario con id 'formProductos'
-    $("#formProductos").submit(function (event) {
-        event.preventDefault(); // Previene el comportamiento por defecto del formulario (enviar la solicitud)
-        // Selecciona todos los campos del formulario que deben ser validados
-        var campos = $("#nombreProducto, #descripcionProducto, #imagenProducto, #precioProducto");
-        var errores = validarCampos(campos); // Llama a la función de validación para los campos seleccionados
-        if (errores.length > 0) { // Verifica si hay errores
-            alert("Completar los siguientes campos: " + errores.join(", ") + " del producto."); // Muestra una alerta con los campos faltantes
-            return false; // Previene el envío del formulario
-        }
-        this.submit(); // Envía el formulario si no hay errores
-    });
-    // Maneja el evento 'click' en el botón con clase 'btn-cancelar'
-    $('.btn-cancelar').on('click', function (event) {
-        event.preventDefault(); // Previene el comportamiento por defecto del botón (enviar una solicitud o seguir un enlace)
-        // Muestra un cuadro de confirmación al usuario
-        if (confirm('¿Estás seguro de que quieres cancelar?')) {
-            // Redirige al usuario a la página principal si confirma la acción
-            window.location.href = '../../index.php';
-        }
-    });
+   // Define una función para validar los campos del formulario
+   function validarCampos(campos) {
+      var errores = []; // Crea un array para almacenar los nombres de los campos con errores
+      // Itera sobre cada campo pasado a la función
+      campos.each(function () {
+         var $this = $(this); // Convierte el elemento actual en un objeto jQuery
+         if ($this.val().trim() === "") {
+            // Verifica si el valor del campo está vacío después de eliminar espacios en blanco
+            $this.addClass("error"); // Agrega la clase 'error' al campo para indicar un error
+            // Agrega el nombre del campo (con formato mejorado) al array de errores
+            errores.push(
+               $this
+                  .attr("name") // Obtiene el valor del atributo 'name' del campo
+                  .replace(/_/g, " ") // Reemplaza los guiones bajos con espacios
+                  .replace(/\b\w/g, (l) => l.toUpperCase()) // Capitaliza la primera letra de cada palabra
+            );
+         } else {
+            $this.removeClass("error"); // Elimina la clase 'error' si el campo no está vacío
+         }
+      });
+      // Devuelve el array de errores
+      return errores;
+   }
+   // Maneja los eventos 'keyup' y 'blur' en los campos de entrada dentro de los formularios especificados
+   $("#formCategorias, #formProductos").on("keyup blur", "input", function () {
+      var $this = $(this); // Convierte el campo de entrada actual en un objeto jQuery
+      if ($this.val().trim() === "") {
+         // Verifica si el campo está vacío después de eliminar espacios en blanco
+         $this.addClass("error"); // Agrega la clase 'error' al campo
+      } else {
+         $this.removeClass("error"); // Elimina la clase 'error' si el campo no está vacío
+      }
+   });
+   // Maneja el evento 'submit' del formulario con id 'formCategorias'
+   $("#formCategorias").submit(function (event) {
+      event.preventDefault(); // Previene el comportamiento por defecto del formulario (enviar la solicitud)
+      var errores = validarCampos($("#nombreCategoria")); // Llama a la función de validación para el campo 'nombreCategoria'
+      if (errores.length > 0) {
+         // Verifica si hay errores
+         alert(
+            "Completar los siguientes campos: " +
+               errores.join(", ") +
+               " de la categoria."
+         ); // Muestra una alerta con los campos faltantes
+         return false; // Previene el envío del formulario
+      }
+      this.submit(); // Envía el formulario si no hay errores
+   });
+   // Maneja el evento 'submit' del formulario con id 'formProductos'
+   $("#formProductos").submit(function (event) {
+      event.preventDefault(); // Previene el comportamiento por defecto del formulario (enviar la solicitud)
+      // Selecciona todos los campos del formulario que deben ser validados
+      var campos = $(
+         "#nombreProducto, #descripcionProducto, #imagenProducto, #precioProducto"
+      );
+      var errores = validarCampos(campos); // Llama a la función de validación para los campos seleccionados
+      if (errores.length > 0) {
+         // Verifica si hay errores
+         alert(
+            "Completar los siguientes campos: " +
+               errores.join(", ") +
+               " del producto."
+         ); // Muestra una alerta con los campos faltantes
+         return false; // Previene el envío del formulario
+      }
+      this.submit(); // Envía el formulario si no hay errores
+   });
+   // Maneja el evento 'click' en el botón con clase 'btn-cancelar'
+   $(".btn-cancelar").on("click", function (event) {
+      event.preventDefault(); // Previene el comportamiento por defecto del botón (enviar una solicitud o seguir un enlace)
+      // Muestra un cuadro de confirmación al usuario
+      if (confirm("¿Estás seguro de que quieres cancelar?")) {
+         // Redirige al usuario a la página principal si confirma la acción
+         window.location.href = "../../index.php";
+      }
+   });
 });
 
 // Función para eliminar una categoría
 function eliminarCategoria() {
-    // Confirmación antes de proceder
-    if (confirm('¿Estás seguro de que deseas eliminar esta categoría?')) { }
+   // Confirmación antes de proceder
+   if (confirm("¿Estás seguro de que deseas eliminar esta categoría?")) {
+   }
 }
 
 // Función para eliminar un producto
 function eliminarProducto() {
-    // Confirmación antes de proceder
-    if (confirm('¿Estás seguro de que deseas eliminar este producto?')) { }
+   // Confirmación antes de proceder
+   if (confirm("¿Estás seguro de que deseas eliminar este producto?")) {
+   }
 }
--- backend/views/productos.php
+++ backend/views/productos.php
@@ -9,75 +9,109 @@ include '../categorias.php';
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <!-- Configura la ventana gráfica para que sea responsive, ajustándose al ancho del dispositivo -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Define el título del documento que se muestra en la pestaña del navegador -->
-    <title>Agregar productos ..:: SiberOS ::..</title>
-    <!-- Vincula el archivo de estilos CSS ubicado en '../../assets/css/estilos.css' -->
-    <link rel="stylesheet" type="text/css" href="../../assets/css/estilos.css">
-    <!-- Incluye la biblioteca jQuery desde una CDN, con una versión específica y una verificación de integridad -->
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
-        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+   <meta charset="UTF-8" />
+   <!-- Configura la ventana gráfica para que sea responsive, ajustándose al ancho del dispositivo -->
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <!-- Define el título del documento que se muestra en la pestaña del navegador -->
+   <title>Agregar productos ..:: SiberOS ::..</title>
+   <!-- Vincula el archivo de estilos CSS ubicado en '../../assets/css/estilos.css' -->
+   <link
+      rel="stylesheet"
+      type="text/css"
+      href="../../assets/css/estilos.css" />
+   <!-- Incluye la biblioteca jQuery desde una CDN, con una versión específica y una verificación de integridad -->
+   <script
+      src="https://code.jquery.com/jquery-3.7.1.min.js"
+      integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+      crossorigin="anonymous"></script>
 </head>
 
 <body>
-    <!-- Enlace que apunta a la página 'index.php' ubicada en el directorio '../../' -->
-    <a href="../../index.php">
-        <!-- Inicio de la sección de encabezado del documento -->
-        <header>
-            <!-- Imagen del logo de la empresa, ubicada en '../../assets/img/logo.png', con texto alternativo y clase 'logo' -->
-            <img src="../../assets/img/logo.png" alt="Logo de la Empresa" class="logo">
-            <!-- Título de nivel 1 que indica el contenido de la página: Agregar nuevos Productos -->
-            <h1>Agregar nuevos Productos</h1>
-        </header>
-    </a>
-    <!-- Contenedor principal del contenido -->
-    <div class="container">
-        <!-- Formulario para agregar nuevos productos, con identificación 'formProductos', que envía datos a 'productos.php' mediante el método POST y permite la carga de archivos -->
-        <form id="formProductos" action="../productos.php" method="post" enctype="multipart/form-data">
-            <!-- Etiqueta para el campo de entrada del nombre del producto -->
-            <label for="nombreProducto">Nombre del Producto:</label>
-            <!-- Campo de entrada de texto para el nombre del producto, con identificación 'nombreProducto' y nombre 'nombre' -->
-            <input type="text" id="nombreProducto" name="nombre" aria-label="Nombre del Producto">
-            <!-- Etiqueta para el campo de entrada de la descripción del producto -->
-            <label for="descripcionProducto">Descripción del Producto:</label>
-            <!-- Campo de entrada de texto para la descripción del producto, con identificación 'descripcionProducto' y nombre 'descripcion' -->
-            <input type="text" id="descripcionProducto" name="descripcion" aria-label="Descripción del Producto">
-            <!-- Etiqueta para el campo de entrada del precio del producto -->
-            <label for="precioProducto">Precio del Producto:</label>
-            <!-- Campo de entrada numérico para el precio del producto, con identificación 'precioProducto', nombre 'precio' y paso de 0.01 para decimales -->
-            <input type="number" id="precioProducto" name="precio" step="0.01" aria-label="Precio del Producto">
-            <!-- Etiqueta para el campo de entrada de la imagen del producto -->
-            <label for="imagenProducto">Imagen del Producto:</label>
-            <!-- Campo de entrada de archivo para la imagen del producto, con identificación 'imagenProducto', nombre 'imagen' y aceptación de solo archivos de imagen -->
-            <input type="file" id="imagenProducto" name="imagen" accept="image/*" aria-label="Imagen del Producto">
-            <!-- Etiqueta para el campo de selección de la categoría del producto -->
-            <label for="categoriaProducto">Categoría del Producto:</label>
-            <!-- Campo de selección de la categoría del producto, con identificación 'categoriaProducto' y nombre 'categoria_id' -->
-            <select id="categoriaProducto" name="categoria_id" aria-label="Categoría del Producto">
-                <!-- Bucle PHP para recorrer cada categoría y crear una opción en el campo de selección -->
-                <?php foreach ($categorias as $cat) : ?>
-                    <!-- Opción del campo de selección con el valor del ID de la categoría -->
-                    <option value="<?php echo htmlspecialchars($cat['id'], ENT_QUOTES, 'UTF-8'); ?>">
-                        <!-- Muestra el nombre de la categoría -->
-                        <?php echo htmlspecialchars($cat['nombre'], ENT_QUOTES, 'UTF-8'); ?>
-                    </option>
-                    <!-- Fin del bucle PHP -->
-                <?php endforeach; ?>
-            </select>
-            <!-- Botón para enviar el formulario -->
-            <button type="submit">Guardar</button>
-            <!-- Botón para restablecer el formulario, con la clase 'btn-cancelar' -->
-            <button class="btn-cancelar" type="reset">Cancelar</button>
-        </form>
-        <!-- Párrafo con el texto 'Creado por Braulio Ruiz Niñoles' -->
-        <p>Creado por Braulio Ruiz Niñoles</p>
-    </div>
-    <!-- Incluye el archivo JavaScript de validaciones ubicado en '../../assets/js/validaciones.js' -->
-    <script src="../../assets/js/validaciones.js"></script>
-    <!-- Incluye el archivo JavaScript principal ubicado en '../../assets/js/main.js' -->
-    <script src="../../assets/js/main.js"></script>
+   <!-- Enlace que apunta a la página 'index.php' ubicada en el directorio '../../' -->
+   <a href="../../index.php">
+      <!-- Inicio de la sección de encabezado del documento -->
+      <header>
+         <!-- Imagen del logo de la empresa, ubicada en '../../assets/img/logo.png', con texto alternativo y clase 'logo' -->
+         <img
+            src="../../assets/img/logo.png"
+            alt="Logo de la Empresa"
+            class="logo" />
+         <!-- Título de nivel 1 que indica el contenido de la página: Agregar nuevos Productos -->
+         <h1>Agregar nuevos Productos</h1>
+      </header>
+   </a>
+   <!-- Contenedor principal del contenido -->
+   <div class="container">
+      <!-- Formulario para agregar nuevos productos, con identificación 'formProductos', que envía datos a 'productos.php' mediante el método POST y permite la carga de archivos -->
+      <form
+         id="formProductos"
+         action="../productos.php"
+         method="post"
+         enctype="multipart/form-data">
+         <!-- Etiqueta para el campo de entrada del nombre del producto -->
+         <label for="nombreProducto">Nombre del Producto:</label>
+         <!-- Campo de entrada de texto para el nombre del producto, con identificación 'nombreProducto' y nombre 'nombre' -->
+         <input
+            type="text"
+            id="nombreProducto"
+            name="nombre"
+            aria-label="Nombre del Producto" />
+         <!-- Etiqueta para el campo de entrada de la descripción del producto -->
+         <label for="descripcionProducto">Descripción del Producto:</label>
+         <!-- Campo de entrada de texto para la descripción del producto, con identificación 'descripcionProducto' y nombre 'descripcion' -->
+         <input
+            type="text"
+            id="descripcionProducto"
+            name="descripcion"
+            aria-label="Descripción del Producto" />
+         <!-- Etiqueta para el campo de entrada del precio del producto -->
+         <label for="precioProducto">Precio del Producto:</label>
+         <!-- Campo de entrada numérico para el precio del producto, con identificación 'precioProducto', nombre 'precio' y paso de 0.01 para decimales -->
+         <input
+            type="number"
+            id="precioProducto"
+            name="precio"
+            step="0.01"
+            aria-label="Precio del Producto" />
+         <!-- Etiqueta para el campo de entrada de la imagen del producto -->
+         <label for="imagenProducto">Imagen del Producto:</label>
+         <!-- Campo de entrada de archivo para la imagen del producto, con identificación 'imagenProducto', nombre 'imagen' y aceptación de solo archivos de imagen -->
+         <input
+            type="file"
+            id="imagenProducto"
+            name="imagen"
+            accept="image/*"
+            aria-label="Imagen del Producto" />
+         <!-- Etiqueta para el campo de selección de la categoría del producto -->
+         <label for="categoriaProducto">Categoría del Producto:</label>
+         <!-- Campo de selección de la categoría del producto, con identificación 'categoriaProducto' y nombre 'categoria_id' -->
+         <select
+            id="categoriaProducto"
+            name="categoria_id"
+            aria-label="Categoría del Producto">
+            <!-- Bucle PHP para recorrer cada categoría y crear una opción en el campo de selección -->
+            <?php foreach ($categorias as $cat) : ?>
+               <!-- Opción del campo de selección con el valor del ID de la categoría -->
+               <option
+                  value="<?php echo htmlspecialchars($cat['id'], ENT_QUOTES, 'UTF-8'); ?>">
+                  <!-- Muestra el nombre de la categoría -->
+                  <?php echo htmlspecialchars($cat['nombre'], ENT_QUOTES, 'UTF-8'); ?>
+               </option>
+               <!-- Fin del bucle PHP -->
+            <?php endforeach; ?>
+         </select>
+         <!-- Botón para enviar el formulario -->
+         <button type="submit">Guardar</button>
+         <!-- Botón para restablecer el formulario, con la clase 'btn-cancelar' -->
+         <button class="btn-cancelar" type="reset">Cancelar</button>
+      </form>
+      <!-- Párrafo con el texto 'Creado por Braulio Ruiz Niñoles' -->
+      <p>Creado por Braulio Ruiz Niñoles</p>
+   </div>
+   <!-- Incluye el archivo JavaScript de validaciones ubicado en '../../assets/js/validaciones.js' -->
+   <script src="../../assets/js/validaciones.js"></script>
+   <!-- Incluye el archivo JavaScript principal ubicado en '../../assets/js/main.js' -->
+   <script src="../../assets/js/main.js"></script>
 </body>
 
 </html>
